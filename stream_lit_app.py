5a12e15d5d4c987f11efa8e1824108c5561da4d3
Taran-17 <85805491+Taran-17@users.noreply.github.com>
2025-01-24 02:14:02 +0530

    Update stream_lit_app.py

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -317,7 +317,7 @@ def write_journal_entry():
                     [journal_title, journal_entry, timestamp],
                 ).collect()
 
-                st.success("Your journal entry has been saved successfully!, Memories will be updated within a few minutes!")
+                st.success("Your journal entry has been saved successfully!, Memories will be updated within 1 minute!")
             except Exception as e:
                 st.error(f"An error occurred while saving your journal entry: {e}")
 
